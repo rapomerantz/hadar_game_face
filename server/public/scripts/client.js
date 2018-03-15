8d4dc5e905d51970c974f9ccea4c7d2cf65058dd
Atticus Pomerantz <rapomerantz@gmail.com>
2018-03-15 13:45:33 -0500

    done with lecture

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,55 @@ $(document).ready(readyNow);
 
 function readyNow () {
     console.log('document ready');
+    $('#submitButton').on('click', submitGame); 
+    //person access the page & it immediatly loads all games ~ don't need to add a game to see the list
+    getAllGames() 
     
+}
+
+function getAllGames() {
+    $.ajax({
+        type: 'GET', 
+        url: '/game'
+    }).done(function(response){
+        appendToDom(response); //the response is our gameCollection array 
+
+    })
+}
+
+function appendToDom (gameCollection) {
+    $('#gameContent').empty()
+    for (let game of gameCollection) {
+        console.log('in appendToDom, GAME:', game); 
+        let tr = $('<tr></tr>');
+        tr.append('<td>' + game.name + '</td>');
+        tr.append('<td>' + game.cost + '</td>'); 
+        $('#gameContent').append(tr); 
+    }
+
+}
+
+
+function submitGame () {
+    let gameName = $('#gameName').val(); 
+    let cost = $('#cost').val(); 
+    let gameToSend = {name: gameName, cost: cost}; 
+    $.ajax({
+        type: 'POST',
+        data: gameToSend, 
+        url: '/game'
+
+    }).done(function(response){
+        //response from a POST will just be '200' success
+        //the .done functionw WILL NOT RUN until a response comes back from the server 
+        console.log('SUCCESS');
+        getAllGames(); 
+        //based on the status code from the server, AJAX knows which function to call (done vs. fail)
+
+    }).fail (function(response) {
+        //this is an if/else jQuery is running based on the status code 
+        alert('Something went wrong...'); 
+
+    })
 }
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,37 @@
 let express = require('express'); 
 let app = express(); 
 const PORT = 4001; 
+let bodyParser = require('body-parser'); 
+const gameCollection = []; //contains objects {name: 'game name', cost: 0.99}
+
+
+//configures the bodyParser for jQuery 
+//MUST BE PUT BEFORE APP.POST
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 //Server static files
 app.use(express.static('server/public')); 
+
+
+
+
+//send back all the games 
+app.get('/game', (req, res) => {
+    res.send(gameCollection); 
+});
+
+//when we want to add a new game
+app.post('/game', (req, res) => {
+    console.log(req.body); 
+    let gameToAdd = req.body; //properties of body.name & body.cost
+    gameCollection.push(gameToAdd); 
+    console.log(gameCollection);
+    
+    res.sendStatus(200); //<---- success status code 
+
+});
+
 
 //spin up the server
 app.listen(PORT, () => {
